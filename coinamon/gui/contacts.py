7ea4ec8d76122d8eacc121c6da59c46777d91775
Jiří Janoušek <janousek.jiri@gmail.com>
2014-12-27 10:51:37 +0100

    Refactor ContactView to multiple classes
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- coinamon/gui/contacts.py
+++ coinamon/gui/contacts.py
@@ -27,43 +27,81 @@ from .view import View
 from ..models import Group, Address
 
 
-class ContactsView(View):
+class ContactsModel(Gtk.TreeStore):
+    GROUP_ID, KEY, KEY_EDITABLE, LABEL, LABEL_EDITABLE, BALANCE, N_TX = range(7)
+
     def __init__(self, db_session):
-        widget = Gtk.ScrolledWindow()
-        super().__init__("contacts", "Contacts", widget)
+        Gtk.TreeStore.__init__(self, int, str, bool, str, bool, str, int)
         self.db_session = db_session
-        self.model = model = Gtk.TreeStore(int, str, str, str, int)
-        with db_session() as dbs:
+
+    def load_data(self):
+        self.clear()
+        with self.db_session() as dbs:
             def add(parent_id=None, parent_iter=None):
                 for group in dbs.query(Group).filter_by(parent_id=parent_id).order_by(Group.name):
-                    print(group)
-                    row_iter = model.append(parent_iter, row=(
-                        group.id, group.name, None, str(group.balance), group.n_tx))
-                    if group.parent_id is not None:
-                        add(self, group.parent_id, row_iter)
-
+                    row_iter = self.append(parent_iter, row=(
+                        group.id, group.name, False, None, False, str(group.balance), group.n_tx))
+                    add(group.id, row_iter)
                     addresses = dbs.query(Address).filter_by(group_id=group.id) \
                         .order_by(Address.id, Address.label)
                     for addr in addresses:
-                        print(addr)
-                        model.append(row_iter, row=(
-                            0, addr.id, addr.label, str(addr.balance), addr.n_tx))
+                        self.append(row_iter, row=(
+                            0, addr.id, False, addr.label, False, str(addr.balance), addr.n_tx))
 
             add()
 
-        self.tree = tree = Gtk.TreeView(model)
-        renderer = Gtk.CellRendererText()
-        column = Gtk.TreeViewColumn("Group/Address", renderer, text=1)
-        tree.append_column(column)
-        renderer = Gtk.CellRendererText()
-        column = Gtk.TreeViewColumn("Label", renderer, text=2)
-        tree.append_column(column)
-        renderer = Gtk.CellRendererText()
-        column = Gtk.TreeViewColumn("Balance", renderer, text=3)
-        tree.append_column(column)
-        renderer = Gtk.CellRendererText()
-        column = Gtk.TreeViewColumn("Tx", renderer, text=4)
-        tree.append_column(column)
+    def is_group(self, path):
+        return path is not None and self[path][ContactsModel.GROUP_ID] != 0
 
-        self.widget.add(tree)
-        self.tree.show()
+    def is_address(self, path):
+        return path is not None and self[path][ContactsModel.GROUP_ID] == 0
+
+
+class BaseContactsTree(Gtk.TreeView):
+    def __init__(self, model):
+        super().__init__(model=model)
+        self.define_columns()
+
+    def define_columns(self):
+        raise NotImplementedError(
+            "{}.define_columns not implemented".format(self.__class__.__name__))
+
+
+class ContactsTree(BaseContactsTree):
+    def define_columns(self):
+        # Group name/address id column
+        renderer = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn("Group/Address", renderer, text=ContactsModel.KEY)
+        column.add_attribute(renderer, "editable", ContactsModel.KEY_EDITABLE)
+        self.append_column(column)
+
+        # Label column
+        renderer = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn("Label", renderer, text=ContactsModel.LABEL)
+        column.add_attribute(renderer, "editable", ContactsModel.LABEL_EDITABLE)
+        self.append_column(column)
+
+        # Balance column
+        renderer = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn("Balance", renderer, text=ContactsModel.BALANCE)
+        self.append_column(column)
+
+        # N tx column
+        renderer = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn("Tx", renderer, text=ContactsModel.N_TX)
+        self.append_column(column)
+
+
+class ContactsView(View):
+    def __init__(self, db_session):
+        widget = grid = Gtk.Grid(orientation=Gtk.Orientation.VERTICAL, row_spacing=10, margin=10)
+        super().__init__("contacts", "Contacts", widget)
+        self.model = model = ContactsModel(db_session)
+        model.load_data()
+        self.tree = tree = ContactsTree(model)
+        scroll = Gtk.ScrolledWindow(vexpand=True, hexpand=True)
+        scroll.add(tree)
+        grid.add(scroll)
+        grid.show_all()
+        self.selection = tree.get_selection()
+        self.selection.set_mode(Gtk.SelectionMode.SINGLE)
