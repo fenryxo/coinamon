2a6516b17d39d54c326cc61068d2e1b410530f54
Jiří Janoušek <janousek.jiri@gmail.com>
2014-12-30 19:09:01 +0100

    Add blockchaininfo module with importer
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

---
+++ coinamon/blockchaininfo/importer.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+# Copyright 2014 Jiří Janoušek <janousek.jiri@gmail.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import json
+
+from coinamon.core.importer import BadDataError
+from coinamon.core.importer import Importer
+
+
+class BlockchaininfoImporter(Importer):
+    name = "blockchaininfo"
+    label = "Blockchain.info Wallet"
+    capabilities = (Importer.IMPORTS_CONTACTS, )
+
+    def import_contacts(self, data, errors):
+        try:
+            data = json.loads(data)["address_book"]
+        except (TypeError, ValueError, LookupError) as e:
+            raise BadDataError(*e.args)
+
+        try:
+            for item in data:
+                try:
+                    addr = item["addr"]
+                    label = item.get("label", addr)
+                    yield {"addr": addr, "label": label}
+                except (LookupError, TypeError) as e:
+                    errors.append(BadDataError(*e.args))
+        except TypeError as e:
+            raise BadDataError(*e.args)
---
+++ coinamon/blockchaininfo/tests/data.py
@@ -0,0 +1,96 @@
+WALLET_JSON = """
+{
+  "guid" : "9daaf2fd-7ab4-4c87-b989-afbd0610ef09",
+  "keys" : [
+    {
+      "addr":"1KjC3LHF1QFH6BCWgvgQfiBkwi8jnS4FSo",
+      "label":"Main"
+    },
+    {
+      "addr":"1PMSy41r6jciuavXLBuearb38H4aSfVKdJ",
+      "label":"Accomodation"
+    },
+    {
+      "addr" : "1G25qfo8aH1SHRfy5VuVYitH9vevyfvse7"
+    },
+    {
+      "addr":"19kvKWCxq7LiWoJD2pTgVkUo9i6XyPjWqn",
+      "label":"Furniture"
+    },
+    {
+      "addr":"19cgUPtsuYAmrkbGxq7kdf44cezdNdMs3z",
+      "label":"Food"
+    },
+    {
+      "addr":"16GRUrUWzMgqNR73Qn12B37TbSzeQZua5u",
+      "label":"Bills"
+    },
+    {
+      "addr":"1B1c4RYhtLDajvc2rAGB8XzomXnJs8cspv",
+      "label":"Test"
+    }
+  ],
+  "address_book" : [
+    {
+      "addrX" : "1Pvt1GNb92W35gXshzwUCG5kpPsBg4ACtA",
+      "label" : "Happy Daisy"
+    },
+    {
+      "addr" : "1Pvt1GNb92W35gXshzwUCG5kpPsBg4ACtA",
+      "label" : "Happy Daisy"
+    },
+    {
+      "addr" : "19hz4WsAEKdP1fcumazpyBYrt8RMNNJium",
+      "label" : "Bill Gates"
+    },
+    {
+      "addr" : "1KopQ3e9pFKEwTmriSvia1TyJc1VJ5KXLF",
+      "label" : "Bitcoin Master"
+    },
+    {
+      "addr" : "1ABnFxaVGQ1ccefekKTXUVPNBiTLy9ytD6",
+      "label" : "Master Yoda"
+    },
+    {
+      "addr" : "1G25qfo8aH1SHRfy5VuVYitH9vevyfvse7",
+      "label" : "Prince of Persia"
+    },
+    {
+      "addr" : "1AhB4xTDhFt2hdDeUajP7gnt7soFwYiVL9"
+    }
+  ],
+  "tx_notes" : {
+    "4bedf2c840707969cf8d94b3e8ac04d6fa1d500cbcb0f89da98486683dcb4da1":
+      "Moved some funds to new wallet."
+  }
+}
+
+"""
+
+CONTACTS = \
+    [
+        {
+            "addr": "1Pvt1GNb92W35gXshzwUCG5kpPsBg4ACtA",
+            "label": "Happy Daisy"
+        },
+        {
+            "addr": "19hz4WsAEKdP1fcumazpyBYrt8RMNNJium",
+            "label": "Bill Gates"
+        },
+        {
+            "addr": "1KopQ3e9pFKEwTmriSvia1TyJc1VJ5KXLF",
+            "label": "Bitcoin Master"
+        },
+        {
+            "addr": "1ABnFxaVGQ1ccefekKTXUVPNBiTLy9ytD6",
+            "label": "Master Yoda"
+        },
+        {
+            "addr": "1G25qfo8aH1SHRfy5VuVYitH9vevyfvse7",
+            "label": "Prince of Persia"
+        },
+        {
+            "addr": "1AhB4xTDhFt2hdDeUajP7gnt7soFwYiVL9",
+            "label": "1AhB4xTDhFt2hdDeUajP7gnt7soFwYiVL9"
+        }
+    ]
